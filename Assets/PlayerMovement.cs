c78c9218d6e90f8cff5e879a10470186d61d9cf5
Abe Shmookler <abe.shmookler@stainedsteelinc.com>
2013-04-10 19:34:46 -0700

    added jump

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	
 	CharacterController characterController;
-	public float horizontalMovementSpeed = 5f;
+	public float horizontalMovementSpeed = 5f, verticalMovementSpeed = 30f;
 	public float gravity = 10f;
 	Vector3 movementVector;
 	
 	// Use this for initialization
 	void Start () {
 		characterController = GetComponent<CharacterController>();
-		
+		movementVector.y = -gravity;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		movementVector = new Vector3(Input.GetAxis("Horizontal") * horizontalMovementSpeed, -gravity, 0); 
+		movementVector = new Vector3(Input.GetAxis("Horizontal") * horizontalMovementSpeed, Input.GetAxis("Vertical") * verticalMovementSpeed, 0); 
+		if (Input.GetAxis("Vertical") == 0)
+		{
+			movementVector.y = -25;
+		}
 		
 		movementVector = movementVector * Time.deltaTime;
 		characterController.Move(movementVector);
